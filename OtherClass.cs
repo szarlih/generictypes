f7f1be673bf77156afda0f66cd9d2750a36ee83d
szarlih <jaroslaw.mielewski@sharpro.pl>
2018-10-09 17:54:00 +0200

    class with other generic name

---
+++ OtherClass.cs
@@ -0,0 +1,14 @@
+﻿namespace GenericDemo
+{
+    using System.Collections.Generic;
+
+    class OtherClass<magic>
+    {
+        private List<magic> list;
+
+        OtherClass()
+        {
+            list = new List<magic>();
+        }
+    }
+}
