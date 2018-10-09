7fcb0f021697c5c2cbd0a15a387c3d98dd1f8b3b
szarlih <39311561+szarlih@users.noreply.github.com>
2018-10-09 14:21:25 +0200

    Add files via upload
    generic initial solution

---
+++ ListMagician.cs
@@ -0,0 +1,38 @@
+﻿namespace GenericDemo
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class ListMagician<T>
+    {
+        private List<T> list;
+        public ListMagician()
+        {
+            list = new List<T>();
+        }
+
+        public void Add(T input)
+        {
+            list.Add(input);
+        }
+
+        public void Swap(T source, T target)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            if(!list.Any())
+            {
+                return "Empty";
+            }
+
+            StringBuilder listAsString = new StringBuilder();
+            list.ForEach(l => listAsString.Append(string.Format("{0} ", l)));
+
+            return listAsString.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace GenericDemo
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListMagician<int> list = new ListMagician<int>();
+            for (int i = 1; i <= 5; i++)
+            {
+                list.Add(i);
+            }
+            Console.WriteLine(list);
+
+            ListMagician<double> sList = new ListMagician<double>();
+            for (int i = 1; i <= 5; i++)
+            {
+                sList.Add(0.1 + i);
+            }
+            Console.WriteLine(sList);
+
+            Console.ReadLine();
+        }
+    }
+}
